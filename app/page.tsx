310fc1a3608d5a5a416353f070e57ab832b51778
hks3333 <harikris177@gmail.com>
2025-04-09 21:48:35 +0530

    f3

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Header } from "@/components/header"
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="bg-green-200 flex h-screen w-screen min-h-[700px]">      
-      <Header />
+    <div className="bg-green-200 flex h-screen w-screen min-h-[700px]">   
+      Hi   
     </div>
   );
 }
